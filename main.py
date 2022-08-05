1e27013b73f02fdf792264bfa9763c1ad22198b5
Arek Lewicki <lewicki1arek@gmail.com>
2022-08-05 15:07:40 +0200

    Only save

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
 
-import sys
+
 import logging
-import operator
-import math
+logging.basicConfig(level=logging.DEBUG)
 
 
 
-def add(*args):
-
-    return sum(args)
-
-def subtract(x, y):
-    
-    if type(x) == int and type(y) == int:
-         print("Podane wartości są liczbami!")
-             
-    score=x-y
-    return print('Wynik odejmowania to: %s' % score)
-     
+#Funkcja mnożąca
 def multiply(*args):
-    result = 1
-    
+    result = 1     
     for x in args:
         result *= x
-    return result   
+    return result
 
+#Funkcja dodająca
+def add(*args):
+    return sum(args)
 
-print(multiply(13,12))
+#Funkcja odejmująca
+def subtract(x, y):
+    return x-y
 
+#Funkcja dzieląca:
+def divide(x, y):
+    return x/y
+
+#Funkcja sprawdzająca argumenty    
+def check(*args):
+        
+    for i in args:
+        if type(i) == str:
+            print(f"Argument {i} jest stringiem")
+            print("Obliczenie nie jest możliwe")
+            break
+        else:
+            print(f"Argument {i} jest liczbą")
+
+      
+                
+
+logging.info("Witaj, ten program pomoże Tobie wykonać obliczenia. Wybierz rodzaj:)")
+choice=input("(1)Dodawanie,(2)Odejmowanie,(3)Mnożenie,(4)Dzielenie :")
+if choice == '1':
+    logging.info("Ok czyli dodajemy")
+    logging.info(numbers=input("Podaj liczby które chcesz do siebie dodać"))
     
+if choice == '2':
+    print("Ok a wiec odejmujmy")
+    x=int(input("Podaj pierwsza liczbę:"))
+    y=int(input("Podaj druga liczbe:"))
+    
+    check(x,y) 
+    logging.info(f"Odejmuje:{x} i {y}" )
+ 
+    logging.debug("Wynik to: %d" % subtract(x,y))
+
+#print(example)
