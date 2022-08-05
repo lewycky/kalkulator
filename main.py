9c9ca2a1ff5392f38cb51b84b356d39e44ada6b1
Arek Lewicki <lewicki1arek@gmail.com>
2022-08-05 19:00:54 +0200

    First version all done

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 
-
-
 import logging
-from unittest import result
+
 logging.basicConfig(level=logging.DEBUG)
 
-
-
 #Funkcja mnożąca
-def multiply(*args):
+def multiply(args):
     result = 1     
     for x in args:
         result *= x
     return result
 
 #Funkcja dodająca
-def add(*args):
-    logging.info("Ok czyli dodajemy")
-    n=int(input("Ile liczb bedziesz dodawał: "))
-    list1 =[]
-    for i in range(n):
-        numbers= int(input(f"Liczba {i+1}:"))
-        list1.append(numbers)
-    return print(f"Wynik to {sum(list1)}")
+def add(args):
+    return sum(args)
 
 #Funkcja odejmująca
-def subtract(x, y):
+def subtract(x,y):
     return x-y
 
 #Funkcja dzieląca:
@@ -43,23 +33,55 @@ def check(*args):
         else:
             print(f"Argument {i} jest liczbą")
 
-      
-                
 
-logging.info("Witaj, ten program pomoże Tobie wykonać obliczenia. Wybierz rodzaj:)")
-choice=input("(1)Dodawanie,(2)Odejmowanie,(3)Mnożenie,(4)Dzielenie :")
+if __name__== "__main__":
+    logging.info("Witaj, ten program pomoże Tobie wykonać obliczenia. Wybierz rodzaj:)")
+    choice=input("(1)Dodawanie,(2)Odejmowanie,(3)Mnożenie,(4)Dzielenie :")
 if choice == '1':
-    result=add()
-    
-    
+
+    logging.info("Ok czyli dodajemy")
+    n=int(input("Ile liczb bedziesz dodawał: "))
+    list1 =[]
+
+    for i in range(n):
+        numbers= float(input(f"Liczba {i+1}:"))
+        list1.append(numbers) 
+
+    logging.debug(f"Wynik to {add(list1)}")   
+        
+  
 if choice == '2':
     logging.info("Ok a wiec odejmujmy")
-    x=int(input("Podaj pierwsza liczbę:"))
-    y=int(input("Podaj druga liczbe:"))
-    
+    x=float(input("Podaj pierwsza liczbę:"))
+    y=float(input("Podaj druga liczbe:"))
     check(x,y) 
     logging.info(f"Odejmuje:{x} i {y}" )
- 
     logging.debug("Wynik to: %d" % subtract(x,y))
+
+if choice == '3': 
+    logging.info("Ok bierzemy sie za mnożenie")
+    n=int(input("Ile liczb bedziesz mnożył: "))
+    list2 =[]
+
+    for i in range(n):
+        numbers= float(input(f"Liczba {i+1}:"))
+        list2.append(numbers) 
+
+    logging.debug("Wynik to: %d" % multiply(list2))
+  
+if choice == '4':
+    logging.info("Ok dzielimy")
+    x=float(input("Podaj pierwsza liczbę:"))
+    y=float(input("Podaj druga liczbe:"))
+    check(x,y) 
+    logging.info(f"Dzielę: {x} i {y}" )
+    logging.debug("Wynik to: %d" % divide(x,y))
+       
+    
+    
+    
+    
+ 
+    
 
 
